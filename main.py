e98d85b5186d8610fd15367a4e5bb64e981d3847
AlexBrou <alexandrepilarbrou@gmail.com>
2019-09-21 08:44:36 +0100

    code organized and commented

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pulp import *
+from pulp import LpProblem, lpSum, LpMinimize, LpVariable, LpInteger, LpStatus, value
 
 
 def shortestPath(edges_with_cost, start_point, end_point, directed=True):
@@ -7,22 +7,34 @@ def shortestPath(edges_with_cost, start_point, end_point, directed=True):
         "Shortest Path between 2 points with Linear Programming", LpMinimize
     )
 
-    flippedEdges = []
+    # this section transforms the directed graph into undirected by duplicating the edges and giving the doubled ones the opposite direction
     if not directed:
-
+        flippedEdges = []
         for s, e, c in edges_with_cost:
             flippedEdges.append([e, s, c])
         edges_with_cost += flippedEdges
+    # end of section
 
+    # arrays of variables
+    # each variable is binary and is respective to an edge (either if it is part of the solution or not)
+
+    # this array will have all the variables multiplied by the cost
     toSumForObjectiveFunction = []
+    # this dictionary will have arrays of all the variables, categorized by their starting point, except the starting point of the path
     variablesThatStartWith = {}
+    # same as the one above, but categorized by the ending point, excluding the ending point of the path
     variablesThatEndWith = {}
 
+    # all the variables for the edges that have starting point equal to the starting point of the path
     all_start_vars = []
+    # same as above, but for the endings
     all_end_vars = []
 
     for s, e, c in edges_with_cost:
+        # binary variable declared
         thisVar = LpVariable("BinaryVar_" + str(s) + "_" + str(e), 0, 1, LpInteger)
+
+        # now, we place the variable in the respective arrays and dictionaries
         toSumForObjectiveFunction.append(thisVar * c)
 
         if s == start_point:
@@ -41,9 +53,16 @@ def shortestPath(edges_with_cost, start_point, end_point, directed=True):
             else:
                 variablesThatEndWith[e] = [thisVar]
 
+    # CONSTRAINTS
+
+    # there must be only 1 edge coming from the starting point of the path
     prob += lpSum(all_start_vars) == 1
+
+    # there must be only 1 edge entering the end point of the path
     prob += lpSum(all_end_vars) == 1
+
     for kk in variablesThatEndWith.keys():
+        # this section makes sure that the program works if the input graph has more than 1 connected component
         if kk in variablesThatEndWith.keys():
             endArray = variablesThatEndWith[kk]
         else:
@@ -52,8 +71,12 @@ def shortestPath(edges_with_cost, start_point, end_point, directed=True):
             startArray = variablesThatStartWith[kk]
         else:
             startArray = []
-        # prob += lpSum(variablesThatEndWith[kk]) == lpSum(variablesThatStartWith[kk])
+        # end of section
+
+        # if there is an edge entering a point, there must be an edge exiting it ( except for the ending point)
         prob += lpSum(endArray) == lpSum(startArray)
+
+    # OBJECTIVE FUNCTION
 
     prob += lpSum(toSumForObjectiveFunction)
 
